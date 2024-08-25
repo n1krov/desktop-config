643e48f0eda3d78e45a799878792603298f172d9
Z0SO <109922256+Z0SO@users.noreply.github.com>
2024-08-25 01:14:47 -0300

    Update keymaps.lua

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 -- ~/.config/nvim/lua/core/keymaps.lua
 
@@ -204,32 +204,4 @@ vim.api.nvim_set_keymap('n', '<Leader><Leader>s', '<Plug>(easymotion-sn)', { nor
 
 vim.cmd [[highlight EasyMotionTarget cterm=bold ctermbg=red guibg=red]]
 vim.cmd [[highlight EasyMotionShade ctermbg=darkgray guibg=darkgray]]
-=======
--- ~/.config/nvim/lua/core/keymaps.lua
 
--- Define la barra espaciadora como la tecla leader
-vim.g.mapleader = ' '
-
--- Mapea <Leader>e para abrir/cierrar nvim-tree
-vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>ew', ':NvimTreeFocus<CR>', { noremap = true, silent = true })
->>>>>>> 06facd8ab114eef4773f7c9b0fb04dcb18cee616
-
-
-
-
-<<<<<<< HEAD
-=======
-
--- Mapea <Leader>ff para buscar archivos
-vim.api.nvim_set_keymap('n', '<Leader>ff', '<cmd>Telescope find_files<CR>', { noremap = true, silent = true })
-
--- Mapea <Leader>fg para buscar en el contenido de archivos
-vim.api.nvim_set_keymap('n', '<Leader>fg', '<cmd>Telescope live_grep<CR>', { noremap = true, silent = true })
-
--- Mapea <Leader>fb para buscar buffers
-vim.api.nvim_set_keymap('n', '<Leader>fb', '<cmd>Telescope buffers<CR>', { noremap = true, silent = true })
-
--- Mapea <Leader>fh para buscar ayuda
-vim.api.nvim_set_keymap('n', '<Leader>fh', '<cmd>Telescope help_tags<CR>', { noremap = true, silent = true })
->>>>>>> 06facd8ab114eef4773f7c9b0fb04dcb18cee616
