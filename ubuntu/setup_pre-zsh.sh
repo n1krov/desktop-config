f9b390457a88064c1cadd403df5e65845526e71d
Z0SO <lautarorobales31@gmail.com>
2025-07-22 10:19:02 -0300

    ubuntu configs

--- ubuntu/setup_pre-zsh.sh
+++ ubuntu/setup_pre-zsh.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+tput civis
 
 # Colores para mensajes
 GREEN='\033[0;32m'
@@ -6,33 +7,51 @@ CYAN='\033[0;36m'
 YELLOW='\033[1;33m'
 END='\033[0m'
 
-echo -e "\n${CYAN}💻 Instalando Neovim...${END}"
+trap ctrl_c INT
 
-# Descargar la última versión estable de Neovim
-wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage -O nvim.appimage
-chmod u+x nvim.appimage
 
-# Mover a /usr/local/bin para agregar al PATH
-sudo mv nvim.appimage /usr/local/bin/nvim
+function ctrl_c() {
+    echo -e "\n${YELLOW}⚠️ Saliendo del script...${END}"
+    tput cnorm
+    exit 1
+}
 
-echo -e "\n${GREEN}✅ Neovim instalado correctamente. Ejecuta 'nvim' para comprobar.${END}"
 
-# ----------------------------------------------------------
+function main() {
 
-echo -e "\n${CYAN}💻 Instalando Rust...${END}"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
+    echo -e "\n${CYAN}💻 Instalando Neovim...${END}"
 
-echo -e "\n${GREEN}✅ Rust instalado correctamente.${END}"
+    # Descargar la última versión estable de Neovim
+    wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage -O nvim.appimage
+    chmod u+x nvim.appimage
 
-# ----------------------------------------------------------
+    # Mover a /usr/local/bin para agregar al PATH
+    sudo mv nvim.appimage /usr/local/bin/nvim
 
-echo -e "\n${CYAN}💻 Instalando Batcat...${END}"
-cargo install bat
+    echo -e "\n${GREEN}✅ Neovim instalado correctamente. Ejecuta 'nvim' para comprobar.${END}"
 
-echo -e "\n${GREEN}✅ Batcat instalado correctamente. Ejecuta 'bat --version' para comprobar.${END}"
+    # ----------------------------------------------------------
 
-# ----------------------------------------------------------
+    # echo -e "\n${CYAN}💻 Instalando Rust...${END}"
+    # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    # source $HOME/.cargo/env
 
-echo -e "\n${CYAN}💻 Instalación completada.${END}"
-echo -e "${YELLOW}⚠️ Nota: Asegúrate de reiniciar la terminal para que Rust se configure correctamente en el PATH.${END}"
+    # echo -e "\n${GREEN}✅ Rust instalado correctamente.${END}"
+    #
+    # echo -e "${YELLOW}⚠️ Nota: Asegúrate de reiniciar la terminal para que Rust se configure correctamente en el PATH.${END}"
+
+    # ----------------------------------------------------------
+
+    echo -e "\n${CYAN}💻 Instalando Batcat y LSD...${END}"
+    sudo apt install -y bat lsd
+
+    echo -e "\n${GREEN}✅ Batcat y LSD instalado correctamente. Ejecuta 'bat/lsd --version' para comprobar.${END}"
+
+    # ----------------------------------------------------------
+
+    echo -e "\n${CYAN}💻 Instalación completada.${END}"
+
+    tput cnorm
+}
+
+main
